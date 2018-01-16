e255f024df8c6176744ec0ac674a0cb68c825a8c
Tiago Do Couto <cs.couto@outlook.com>
2018-01-15 22:02:41 -0600

    S01.02-Exercise-AddWeatherList

--- app/src/main/java/com/coutocode/sunshine/MainActivity.java
+++ app/src/main/java/com/coutocode/sunshine/MainActivity.java
@@ -2,12 +2,25 @@ package com.coutocode.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.coutocode.sunshine.utilities.SunshineWeatherUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mWeathertextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWeathertextView = findViewById(R.id.tv_weather_data);
+
+        String[] weatherForecast = {"test1", "test2"};
+
+        for (String weather: weatherForecast){
+            mWeathertextView.append(weather + "\n\n\n");
+        }
     }
 }
