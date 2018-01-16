3474bbf00caff491976ffdb17d9629a089dd8d47
Tiago Do Couto <cs.couto@outlook.com>
2018-01-16 11:30:59 -0600

    S02.03-Exercise-Polish

--- app/src/main/java/com/coutocode/sunshine/MainActivity.java
+++ app/src/main/java/com/coutocode/sunshine/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.coutocode.sunshine.data.SunshinePreferences;
 import com.coutocode.sunshine.utilities.NetworkUtils;
+import com.coutocode.sunshine.utilities.OpenWeatherJsonUtils;
 import com.coutocode.sunshine.utilities.SunshineWeatherUtils;
 
 import java.io.IOException;
@@ -16,12 +20,20 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
 
+    TextView mWeatherTextView;
+    TextView mErrorTextView;
+    ProgressBar mWeatherProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadWeatherData("");
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+        mErrorTextView = (TextView) findViewById(R.id.tv_error_message);
+        mWeatherProgressBar = (ProgressBar) findViewById(R.id.pb_weather);
+
+        loadWeatherData();
     }
 
     @Override
@@ -34,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.action_refresh:
+                mWeatherTextView.setText("");
+                loadWeatherData();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -41,29 +55,70 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class RequestWeatherTask extends AsyncTask<URL, Void, String>{
+    class FetchWeatherTask extends AsyncTask<String, Void, String[]>{
 
         @Override
-        protected String doInBackground(URL... urls) {
-            URL searchUrl = urls[0];
-            String results = null;
-            try {
-                results = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return results;
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mWeatherProgressBar.setVisibility(View.VISIBLE);
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            //display the data
-            super.onPostExecute(s);
+        protected String[] doInBackground(String... urls) {
+            if (urls.length == 0) {
+                return null;
+            }
+
+            String location = urls[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                String jsonWeatherResponse = NetworkUtils
+                        .getResponseFromHttpUrl(weatherRequestUrl);
+
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleJsonWeatherData;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+            mWeatherProgressBar.setVisibility(View.INVISIBLE);
+
+            if (weatherData != null) {
+                showWeatherDataView();
+                /*
+                 * Iterate through the array and append the Strings to the TextView. The reason why we add
+                 * the "\n\n\n" after the String is to give visual separation between each String in the
+                 * TextView. Later, we'll learn about a better way to display lists of data.
+                 */
+                for (String weatherString : weatherData) {
+                    mWeatherTextView.append((weatherString) + "\n\n\n");
+                }
+            }else{
+                showErrorMessage();
+            }
         }
     }
 
-    void loadWeatherData(String location){
-        URL locationWeather = NetworkUtils.buildUrl(location);
-        new RequestWeatherTask().execute(locationWeather);
+    void loadWeatherData(){
+        showWeatherDataView();
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    void showWeatherDataView(){
+        mErrorTextView.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
+    void showErrorMessage(){
+        mErrorTextView.setVisibility(View.VISIBLE);
+        mWeatherTextView.setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/com/coutocode/sunshine/utilities/NetworkUtils.java
+++ app/src/main/java/com/coutocode/sunshine/utilities/NetworkUtils.java
@@ -62,12 +62,16 @@ public final class NetworkUtils {
 
 
     public static URL buildUrl(String locationQuery) {
-        Uri uri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                .appendQueryParameter(QUERY_PARAM,locationQuery).build();
+        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
 
         URL url = null;
         try {
-            url = new URL(uri.toString());
+            url = new URL(builtUri.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
