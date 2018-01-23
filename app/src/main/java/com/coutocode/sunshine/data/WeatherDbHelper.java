a740a6fdc48f264cceec4a0346b481422054142b
Tiago Do Couto <cs.couto@outlook.com>
2018-01-22 19:17:18 -0600

    S07.02-Exercise-PreventInvalidInserts

--- app/src/main/java/com/coutocode/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/coutocode/sunshine/data/WeatherDbHelper.java
@@ -4,13 +4,15 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.coutocode.sunshine.data.WeatherContract.WeatherEntry;
+
 /**
  * Created by docouto on 1/22/18.
  */
 
 public class WeatherDbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "weather.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public WeatherDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -18,8 +20,19 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "";
-        sqLiteDatabase.execSQL(sql);
+
+        final String SQL_CREATE_WEATHER_TABLE =
+                "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
+                        WeatherEntry._ID               + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        WeatherEntry.COLUMN_DATE       + " INTEGER, "                 +
+                        WeatherEntry.COLUMN_WEATHER_ID + " INTEGER, "                 +
+                        WeatherEntry.COLUMN_MIN_TEMP   + " REAL, "                    +
+                        WeatherEntry.COLUMN_MAX_TEMP   + " REAL, "                    +
+                        WeatherEntry.COLUMN_HUMIDITY   + " REAL, "                    +
+                        WeatherEntry.COLUMN_PRESSURE   + " REAL, "                    +
+                        WeatherEntry.COLUMN_WIND_SPEED + " REAL, "                    +
+                        WeatherEntry.COLUMN_DEGREES    + " REAL" + ");";
+        sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
     }
 
     @Override
